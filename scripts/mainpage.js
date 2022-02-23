e6b3b54e4a5f68842a7d1cc59b25231dd3ae38d0
Megarion <andy181209x@gmail.com>
2022-02-23 11:32:26 +0700

    💥 ALMOST DONE

--- scripts/mainpage.js
+++ scripts/mainpage.js
@@ -1,14 +1,50 @@
-// Main script
-let tab = "home";
+let projectsVisited = false;
 
-let tabList = [
-    
+let tablist = [
+    "about", "contacts", "projects", "share"
 ];
 
-function tabSwitch(tab) {
-    
+// Utility functions
+function gID(elem){
+    return document.getElementById(elem);
 }
 
-$(document).ready(function () {
-    
+function hide(elem) {
+    gID(elem).style.display = 'none';
+}
+
+function show(params) {
+    gID(params).style.display = 'block';
+}
+
+// Interactive functions
+function hideAllTabs(){
+    for (let i = 0; i < tablist.length; i++) {
+        gID(tablist[i]).style.display = 'none';
+    }
+}
+
+function tabSwitch(tab){
+    hideAllTabs();
+    gID(tab).style.display = 'block';
+}
+
+function aboutHidden(){
+    gID('more').style.display = 'none';
+    gID('less').style.display = 'none';
+}
+
+function aboutExpand(){
+    aboutHidden();
+    gID('more').style.display = 'block';
+}
+
+function aboutCollapse(){
+    aboutHidden();
+    gID('less').style.display = 'block';
+}
+
+// Main code
+window.addEventListener("load", () => {
+    tabSwitch('about');
 });
